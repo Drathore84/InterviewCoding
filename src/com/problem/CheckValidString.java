4f98972fbe6ca2ea9f44cb65949d07d0b5807deb
Dinesh rathore <rathore.dinesh01@gmail.com>
2020-07-16 22:06:43 +0530

    Using Stack

--- src/com/problem/CheckValidString.java
+++ src/com/problem/CheckValidString.java
@@ -1,36 +1,47 @@
 package com.problem;
 
+import java.util.Stack;
+
 public class CheckValidString {
 
 	public boolean balancedParenthesis(String s) {
 
-		if (s.isEmpty())return true;
-		
-		
-		
-		int obCount, cbCount, $Count;
-		obCount = cbCount = $Count = 0;
+		if (s.isEmpty())
+			return true;
+		Stack<Character> stack = new Stack<Character>();
+
+		int obCount, $Count;
+		obCount = $Count = 0;
 
 		for (int i = 0; i < s.length(); i++) {
-
 			char x = s.charAt(i);
-			
-			if(s.charAt(0)==']' || s.charAt(s.length()-1) == '[') return false;
-			if (x == '[')
+			if (s.charAt(0) == ']' || s.charAt(s.length() - 1) == '[')
+				return false;
+
+			switch (x) {
+			case '[':
 				obCount++;
-			else if (x == ']')
-				cbCount++;
-			else if (x == '$')
+				stack.push(x);
+				break;
+			case '$':
+
 				$Count++;
+				stack.push(x);
+				break;
+			case ']':
+				stack.pop();
+				obCount--;
+				break;
 
+			}
 		}
 
-		if (cbCount == obCount)return true;
-		else {
-			if ($Count - (cbCount - obCount) >= 0)return true;
+		if (!stack.isEmpty()) {
+			if (stack.firstElement() == '$' & obCount != 0)
+				return false;
 		}
 
-		return false;
+		return ($Count - obCount >= 0);
 	}
 
 }
